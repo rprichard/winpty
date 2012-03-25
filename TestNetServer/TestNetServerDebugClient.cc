43fac6819bfd1884b873d4258a85fdb86e8cfd49
Ryan Prichard <ryan.prichard@gmail.com>
2012-03-24 17:55:39 -0700

    Remove the obsolete TestNet client/server code.
    It won't work anymore because the agent's interface has changed.

--- TestNetServer/TestNetServerDebugClient.cc
+++
@@ -1,3 +0,0 @@
-// This is a hack.  I want to compile DebugClient.cc into the project,
-// and I don't know how to setup a Makefile to do it.
-#include "../Shared/DebugClient.cc"
--- TestNetServer/main.cc
+++
@@ -1,9 +0,0 @@
-#include <QtCore/QCoreApplication>
-#include "Server.h"
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    Server server;
-    return a.exec();
-}
