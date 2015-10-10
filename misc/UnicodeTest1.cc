1ef25fb6c6c1e96730a23529f365dff73ea082f2
Ryan Prichard <ryan.prichard@gmail.com>
2015-10-10 04:31:58 -0500

    Fix UnicodeTest1 to work with TrueType fonts

--- misc/UnicodeTest1.cc
+++ misc/UnicodeTest1.cc
@@ -15,14 +15,16 @@ int main(int argc, char *argv[]) {
     assert(ret && actual == 1);
 
     // Read it back.
-    CHAR_INFO data;
-    COORD bufSize = {1, 1};
+    CHAR_INFO data[2];
+    COORD bufSize = {2, 1};
     COORD bufCoord = {0, 0};
-    SMALL_RECT topLeft = {0, 0, 0, 0};
+    SMALL_RECT topLeft = {0, 0, 1, 0};
     ret = ReadConsoleOutputW(
-            GetStdHandle(STD_OUTPUT_HANDLE), &data, bufSize, bufCoord, &topLeft);
+            GetStdHandle(STD_OUTPUT_HANDLE), data, bufSize, bufCoord, &topLeft);
     assert(ret);
 
-    printf("CHAR: 0x%04x\n", data.Char.UnicodeChar);
+    printf("\n");
+    printf("CHAR: 0x%04x\n", data[0].Char.UnicodeChar);
+    printf("CHAR: 0x%04x\n", data[1].Char.UnicodeChar);
     return 0;
 }
