55bea514304417d8f9202a9a1a4a51b75ba29e62
Ryan Prichard <ryan.prichard@gmail.com>
2015-10-22 03:04:17 -0500

    Implement precompiled headers in misc/buffer-tests
     * I'm still going to try to include what is used in each module.
    
     * At some point, I should implement these in the main project.  It will
       need more testing on the various compilers.  It really needs to be
       on-by-default everywhere.  If it's off, then at least pch.h should be
       included everywhere, to minimize configuration differences between my
       checkout and other people's checkouts.
    
     * There should be some way to configure it off to check whether includes
       are correct.
    
     * The change reduces build times from 27.6s to 15.2s on a single-core VM.

---
+++ misc/buffer-tests/harness/pch.h
@@ -0,0 +1,19 @@
+// Precompiled Headers.
+//
+// Unfortunate evil that speeds up compile times.  In principle, compilation
+// should still work if PCH is turned off.
+//
+
+#include <windows.h>
+
+#include <algorithm>
+#include <array>
+#include <cstdio>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <memory>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
