38cc0bf3fed55699900cc890a1d5cce0fbc31073
Ryan Prichard <ryan.prichard@gmail.com>
2015-10-22 19:00:00 -0500

    Reorganize buffer-tests/Makefile and permit tests that aren't checked in

---
+++ misc/buffer-tests/tests.mk
@@ -0,0 +1,8 @@
+TESTS += \
+    build/Test_GetConsoleTitleW.exe \
+    build/Win7Bug_InheritHandles.exe \
+    build/Win7Bug_RaceCondition.exe \
+    build/TestHandleInheritance.exe
+
+# To add tests that aren't checked-in, create an mk file of this name:
+-include local_tests.mk
