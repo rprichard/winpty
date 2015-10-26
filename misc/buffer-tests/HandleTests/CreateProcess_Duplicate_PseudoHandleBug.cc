72c5ea52fdbf9da20179402bfe9e975d7a9bb1d9
Ryan Prichard <ryan.prichard@gmail.com>
2015-10-26 18:42:06 -0500

    Fixes and comments to Test_CreateProcess_Duplicate_PseudoHandleBug
    It seems that this bug was fixed in WOW64 prior to its fixing in the
    normal environment.
    
    It makes no sense to run Test_CreateProcess_Duplicate_PseudoHandleBug_IL
    on OS's that don't have inherit lists.

--- misc/buffer-tests/HandleTests/CreateProcess_Duplicate_PseudoHandleBug.cc
+++ misc/buffer-tests/HandleTests/CreateProcess_Duplicate_PseudoHandleBug.cc
@@ -8,6 +8,13 @@
 
 REGISTER(Test_CreateProcess_Duplicate_PseudoHandleBug, always);
 static void Test_CreateProcess_Duplicate_PseudoHandleBug() {
+    // This form of the bug occurs on these server OSs:
+    //  - Server 2003 R2 SP2 64bit (64-bit and WOW64)
+    //  - Server 2008 SP2 64bit (64-bit)
+    //  - Server 2008 R2 SP1 64bit (WOW64)
+    // This form of the bug is fixed on these server OSs:
+    //  - Server 2008 SP2 64bit (WOW64)
+    //  - Server 2008 R2 SP1 64bit (WOW64)
     Worker p;
     Handle::invent(GetCurrentProcess(), p).setStdout();
     auto c = p.child({ false });
@@ -20,7 +27,7 @@ static void Test_CreateProcess_Duplicate_PseudoHandleBug() {
     }
 }
 
-REGISTER(Test_CreateProcess_Duplicate_PseudoHandleBug_IL, always);
+REGISTER(Test_CreateProcess_Duplicate_PseudoHandleBug_IL, isAtLeastVista);
 static void Test_CreateProcess_Duplicate_PseudoHandleBug_IL() {
     // As above, but use an inherit list.  With an inherit list, standard
     // handles are duplicated, but only with Windows 8 and up.
@@ -35,6 +42,8 @@ static void Test_CreateProcess_Duplicate_PseudoHandleBug_IL() {
             auto handleToPInP = Handle::dup(p.processHandle(), p);
             CHECK(compareObjectHandles(c.getStdout(), handleToPInP));
         } else {
+            // Prior to Windows 8, duplication doesn't occur, so the bug isn't
+            // relevant.  We run the test anyway, but it's less interesting.
             CHECK(c.getStdout().value() == INVALID_HANDLE_VALUE);
         }
     }
